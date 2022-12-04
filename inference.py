75fb64f8efe93c6151d7be38b1796cf8c5938930
angseung <angseung@outlook.com>
2022-12-04 19:15:01 +0900

    modified default value of anchors...

--- inference.py
+++ inference.py
@@ -12,11 +12,14 @@ elif "Darwin" in curr_os:
     device = "mps" if torch.backends.mps.is_available() else "cpu"
 elif "Linux" in curr_os:
     device = "cuda" if torch.cuda.is_available() else "cpu"
+else:
+    device = "cpu"
 
 cfg = "./models/yolov3-nano.yaml"
-img_size = 320
-n_classes = 62
-model = Model(cfg=cfg, nc=n_classes, anchors=5).to(device)
+img_size: int = 320
+n_classes: int = 62  # default value is 62
+anchors: int = 3  # default value is 3
+model = Model(cfg=cfg, nc=n_classes, anchors=anchors).to(device)
 model.eval()
 dummy_input = torch.randn((1, 3, img_size, img_size)).to(device)
 [dummy_bboxes, dummy_output] = model(dummy_input)
