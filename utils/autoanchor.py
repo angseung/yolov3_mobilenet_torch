595bc42558b7c68c095281c6e6278b1f58d941ba
angseung <swin@surromind.ai>
2022-12-15 11:34:31 +0900

    Added codes for detecting extremely small objects

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -142,11 +142,22 @@ def kmean_anchors(
     wh0 = np.concatenate([l[:, 3:5] * s for s, l in zip(shapes, dataset.labels)])  # wh
 
     # Filter
-    # TODO: count the number of objects which are < {3, 5, 10, 20, 40} pixels in size
     i = (wh0 < 3.0).any(1).sum()
     if i:
         LOGGER.info(
             f"{PREFIX}WARNING: Extremely small objects found. {i} of {len(wh0)} labels are < 3 pixels in size."
+        )
+
+    i = (wh0 < 10.0).any(1).sum()
+    if i:
+        LOGGER.info(
+            f"{PREFIX}WARNING: Extremely small objects found. {i} of {len(wh0)} labels are < 10 pixels in size."
+        )
+
+    i = (wh0 < 5.0).any(1).sum()
+    if i:
+        LOGGER.info(
+            f"{PREFIX}WARNING: Extremely small objects found. {i} of {len(wh0)} labels are < 5 pixels in size."
         )
     wh = wh0[(wh0 >= 2.0).any(1)]  # filter > 2 pixels
     # wh = wh * (np.random.rand(wh.shape[0], 1) * 0.9 + 0.1)  # multiply by random scale 0-1
