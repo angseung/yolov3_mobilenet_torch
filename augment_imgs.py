297b7e61ee662f5296483b1c00276d3e653b5610
angseung <swim@surromind.ai>
2023-01-05 13:57:34 +0900

    Added an image mixer

--- augment_imgs.py
+++ augment_imgs.py
@@ -1,96 +1,53 @@
 import os
-import shutil
-from typing import List, Tuple
+import random
 import numpy as np
-
-
-def find_draw_region(img: np.ndarray, label: np.ndarray, foreground: np.ndarray) -> Tuple[int]:
-    w, h = img.shape[:2]
-    w_fg, h_fg = foreground.shape[:2]
-    label_pixel = np.copy(label)
-    label_pixel[:, 1, 3] *= w
-    label_pixel[:, 2, 4] *= h
-
-    xtl = label_pixel[:, 1].min()
-    ytl = label_pixel[:, 2].min()
-    xbr = label_pixel[:, 3].max()
-    ybr = label_pixel[:, 4].max()
-
-    region_candidate = [False] * 8
-    region_area = [0.0] * 8
-
-    # region 1
-    w1, h1 = xtl, ytl
-    region_candidate[0] = (w1 >= w) and (h1 >= h)
-    region_area[0] = w1 * h1
-
-    # region 2
-    w2, h2 = xbr - xtl, ytl
-    region_candidate[1] = (w2 >= w) and (h2 >= h)
-    region_area[1] = w2 * h2
-
-    # region 3
-    w3, h3 = w - xbr, ytl
-    region_candidate[2] = (w3 >= w) and (h3 >= h)
-    region_area[2] = w3 * h3
-
-    # region 4
-    w4, h4 = xtl, ybr - ytl
-    region_candidate[3] = (w4 >= w) and (h4 >= h)
-    region_area[3] = w4 * h4
-
-    # region 5
-    w5, h5 = w - xbr, ybr - ytl
-    region_candidate[4] = (w5 >= w) and (h5 >= h)
-    region_area[4] = w5 * h5
-
-    # region 6
-    w6, h6 = xtl, h - ybr
-    region_candidate[5] = (w6 >= w) and (h6 >= h)
-    region_area[5] = w6 * h6
-
-    # region 7
-    w7, h7 = xbr - xtl, h - ybr
-    region_candidate[6] = (w7 >= w) and (h7 >= h)
-    region_area[6] = w7 * h7
-
-    # region 8
-    w8, h8 = w - xbr, h - ybr
-    region_candidate[7] = (w8 >= w) and (h8 >= h)
-    region_area[7] = w8 * h8
-
-    region_candidate = np.array(region_candidate, dtype=np.uint32)
-    region_area = np.array(region_area, dtype=np.uint32)
-
-    selected_region = (region_candidate * region_area).tolist().argmax() + 1
-
-    if selected_region == 1:
-        area = (0, 0, xtl, ytl)
-    elif selected_region == 2:
-        area = (xtl, 0, w - xbr, ytl)
-    elif selected_region == 3:
-        area = (w - xbr, 0, w, ytl)
-    elif selected_region == 4:
-        area = (0, ytl, xtl, h - ybr)
-    elif selected_region == 5:
-        area = (w - xbr, ytl, w, h - ybr)
-    elif selected_region == 6:
-        area = (0, h - ybr, xtl, h - ybr)
-    elif selected_region == 7:
-        area = (xtl, h - ybr, w - xbr, h)
-    elif selected_region == 8:
-        area = (xbr, ybr, w, h)
-
-    return area
+import cv2
+from utils.augment_utils import parse_label, label_yolo2voc, label_voc2yolo, find_draw_region
 
 
 if __name__ == "__main__":
-    img_dir = "/data_yper/yperv1/images/train"
-    label_dir = "/data_yper/yperv1/labels/train"
-    target_dir = "/data_yper/yperv2"
+    img_dir = "./data/yperv1/images/train"
+    label_dir = "./data/yperv1/labels/train"
+    target_dir = "./data/yperv2"
 
     if not os.path.isdir(target_dir):
         os.makedirs(f"{target_dir}/images/train")
         os.makedirs(f"{target_dir}/labels/train")
 
     img_list = os.listdir(img_dir)
+
+    for i, fname in enumerate(img_list[:]):
+        fname = fname[:-4]  # remove suffix and new line
+        bg_label = parse_label(f"{label_dir}/{fname}.txt")
+        bg_img = cv2.imread(f"{img_dir}/{fname}.jpg")
+        bg_h, bg_w = bg_img.shape[:2]
+        fg_img = cv2.imread("Z03ah4432X.jpg")
+        fg_label = parse_label("Z03ah4432X.txt")
+        fg_h, fg_w = fg_img.shape[:2]
+
+        selected_region, area_xtl, area_ytl, area_xbr, area_ybr = find_draw_region(bg_img, bg_label, fg_img)
+        assert area_xbr > area_xtl and area_ybr > area_ytl
+
+        allowed_width = (area_xbr - fg_w).item()
+        allowed_height = (area_ybr - fg_h).item()
+
+        draw_xtl = random.randint(0, allowed_width - 1)
+        draw_ytl = random.randint(0, allowed_height - 1)
+
+        label_voc = label_yolo2voc(bg_label, bg_h, bg_w)
+        label_yolo = label_voc2yolo(label_voc, bg_h, bg_w)
+
+        abs_xtl, abs_ytl = draw_xtl + area_xtl, draw_ytl + area_ytl
+
+        # draw fg_img on bg_img
+        bg_img[abs_ytl : abs_ytl + fg_h, abs_xtl : abs_xtl + fg_w, :] = fg_img[:, :, :]
+
+        # compensate bbox offset of fg_label
+        fg_label_voc = label_yolo2voc(fg_label, fg_h, fg_w)
+        fg_label_voc[:, [1, 3]] += abs_xtl
+        fg_label_voc[:, [2, 4]] += abs_ytl
+        fg_label_yolo = label_voc2yolo(fg_label_voc, bg_h, bg_w)
+
+        cv2.imwrite(f"{target_dir}/images/train/{fname}.jpg", bg_img)
+
+    
---
+++ utils/augment_utils.py
@@ -0,0 +1,146 @@
+from typing import Tuple
+import numpy as np
+
+
+def parse_label(fname: str) -> np.ndarray:
+    with open(fname, encoding="utf-8") as f:
+        bboxes = f.readlines()
+        label = []
+
+    for bbox in bboxes:
+        label.append(bbox.split())
+
+    return np.array(label, dtype=np.float64)
+
+
+def label_yolo2voc(label_yolo: np.ndarray, h: int, w: int) -> np.ndarray:
+    # from: (x_center, y_center, w, h), normalized
+    # to:   (xtl, ytl, xbr, ybr)
+    label_voc = np.zeros(label_yolo.shape, dtype=np.float64)
+    label_voc[:, 0] = label_yolo[:, 0]
+
+    label_yolo_temp = label_yolo.copy()
+    label_yolo_temp[:, [1, 3]] *= w
+    label_yolo_temp[:, [2, 4]] *= h
+
+    # convert x_center, y_center to xtl, ytl
+    label_voc[:, 1] = label_yolo_temp[:, 1] - 0.5 * label_yolo_temp[:, 3]
+    label_voc[:, 2] = label_yolo_temp[:, 2] - 0.5 * label_yolo_temp[:, 4]
+
+    # convert width, height to xbr, ybr
+    label_voc[:, 3] = label_voc[:, 1] + label_yolo_temp[:, 3]
+    label_voc[:, 4] = label_voc[:, 2] + label_yolo_temp[:, 4]
+
+    return label_voc.astype(np.uint32)
+
+
+def label_voc2yolo(label_voc: np.ndarray, h: int, w: int) -> np.ndarray:
+    # from: (xtl, ytl, xbr, ybr)
+    # to:   (x_center, y_center, w, h), normalized
+
+    label_yolo = np.zeros(label_voc.shape, dtype=np.float64)
+    label_yolo[:, 0] = label_voc[:, 0]
+
+    # convert xtl, ytl to x_center, y_center
+    label_yolo[:, 1] = 0.5 * (label_voc[:, 1] + label_voc[:, 3])
+    label_yolo[:, 2] = 0.5 * (label_voc[:, 2] + label_voc[:, 4])
+
+    # convert xbr, ybr to width, height
+    label_yolo[:, 3] = label_voc[:, 3] - label_voc[:, 1]
+    label_yolo[:, 4] = label_voc[:, 4] - label_voc[:, 2]
+
+    # normalize
+    label_yolo[:, [1, 3]] /= w
+    label_yolo[:, [2, 4]] /= h
+
+    return label_yolo
+
+
+def find_draw_region(img: np.ndarray, label: np.ndarray, foreground: np.ndarray) -> Tuple[int]:
+    h, w = img.shape[:2]
+    w_fg, h_fg = foreground.shape[:2]
+    label_pixel = np.copy(label)
+    label_pixel[:, [1, 3]] *= w
+    label_pixel[:, [2, 4]] *= h
+
+    # convert x_center, y_center to xtl, ytl
+    label_pixel[:, 1] = label_pixel[:, 1] - 0.5 * label_pixel[:, 3]
+    label_pixel[:, 2] = label_pixel[:, 2] - 0.5 * label_pixel[:, 4]
+
+    # convert width, height to xbr, ybr
+    label_pixel[:, 3] = label_pixel[:, 1] + label_pixel[:, 3]
+    label_pixel[:, 4] = label_pixel[:, 2] + label_pixel[:, 4]
+
+    label_pixel = label_pixel.astype(np.uint32)
+
+    xtl = label_pixel[:, 1].min()
+    ytl = label_pixel[:, 2].min()
+    xbr = label_pixel[:, 3].max()
+    ybr = label_pixel[:, 4].max()
+
+    region_candidate = [False] * 8
+    region_area = [0.0] * 8
+
+    # region 1
+    w1, h1 = xtl, ytl
+    region_candidate[0] = (w1 >= w_fg) and (h1 >= h_fg)
+    region_area[0] = w1 * h1
+
+    # region 2
+    w2, h2 = xbr - xtl, ytl
+    region_candidate[1] = (w2 >= w_fg) and (h2 >= h_fg)
+    region_area[1] = w2 * h2
+
+    # region 3
+    w3, h3 = w - xbr, ytl
+    region_candidate[2] = (w3 >= w_fg) and (h3 >= h_fg)
+    region_area[2] = w3 * h3
+
+    # region 4
+    w4, h4 = xtl, ybr - ytl
+    region_candidate[3] = (w4 >= w_fg) and (h4 >= h_fg)
+    region_area[3] = w4 * h4
+
+    # region 5
+    w5, h5 = w - xbr, ybr - ytl
+    region_candidate[4] = (w5 >= w_fg) and (h5 >= h_fg)
+    region_area[4] = w5 * h5
+
+    # region 6
+    w6, h6 = xtl, h - ybr
+    region_candidate[5] = (w6 >= w_fg) and (h6 >= h_fg)
+    region_area[5] = w6 * h6
+
+    # region 7
+    w7, h7 = xbr - xtl, h - ybr
+    region_candidate[6] = (w7 >= w_fg) and (h7 >= h_fg)
+    region_area[6] = w7 * h7
+
+    # region 8
+    w8, h8 = w - xbr, h - ybr
+    region_candidate[7] = (w8 >= w_fg) and (h8 >= h_fg)
+    region_area[7] = w8 * h8
+
+    region_candidate = np.array(region_candidate, dtype=np.uint32)
+    region_area = np.array(region_area, dtype=np.uint32)
+
+    selected_region = (region_candidate * region_area).argmax() + 1
+
+    if selected_region == 1:
+        area = (selected_region, 0, 0, xtl, ytl)
+    elif selected_region == 2:
+        area = (selected_region, xtl, 0, xbr, ytl)
+    elif selected_region == 3:
+        area = (selected_region, xbr, 0, w, ytl)
+    elif selected_region == 4:
+        area = (selected_region, 0, ytl, xtl, ybr)
+    elif selected_region == 5:
+        area = (selected_region, xbr, ytl, w, ybr)
+    elif selected_region == 6:
+        area = (selected_region, 0, ybr, xtl, h)
+    elif selected_region == 7:
+        area = (selected_region, xtl, ybr, xbr, h)
+    elif selected_region == 8:
+        area = (selected_region, xbr, ybr, w, h)
+
+    return area
