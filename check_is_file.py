bddd27d91d30eb2ad5d5c406887ac2e22cf294aa
angseung <swim@surromind.ai>
2023-01-06 08:11:32 +0900

    Added a list of yperv1 img file names

--- check_is_file.py
+++ check_is_file.py
@@ -6,6 +6,10 @@ base_dir_labels = "./data/yperv1/labels/train"
 im_list_swim = os.listdir(base_dir)
 im_list_swim_labels = os.listdir(base_dir_labels)
 
+with open("img_list.txt", "w") as f:
+    for fname in im_list_swim:
+        f.write(f"{fname[:-4]}\n")
+
 # with open("total_image_list.txt", "r") as f:
 #     im_list_sjan = f.readlines()
 
@@ -29,23 +33,23 @@ im_list_swim_labels = os.listdir(base_dir_labels)
 #     if not os.path.isfile(f"./data/yperv1/images/train/{fname[:-4]}.jpg"):
 #         print(fname)
 
-with open("drop_list.txt", encoding="utf-8") as f:
-    f_list = f.readlines()
-
-for fname in f_list:
-    fname = fname[:-1]
-
-    if not os.path.isdir("/data_yper/yperv1/images/deleted"):
-        os.makedirs("/data_yper/yperv1/images/deleted")
-
-    shutil.move(
-        f"/data_yper/yperv1/images/train/{fname}.jpg",
-        f"/data_yper/yperv1/images/deleted/{fname}.jpg",
-    )
-    shutil.move(
-        f"/data_yper/yperv1/labels/train/{fname}.txt",
-        f"/data_yper/yperv1/labels/deleted/{fname}.txt",
-    )
+# with open("drop_list.txt", encoding="utf-8") as f:
+#     f_list = f.readlines()
+#
+# for fname in f_list:
+#     fname = fname[:-1]
+#
+#     if not os.path.isdir("/data_yper/yperv1/images/deleted"):
+#         os.makedirs("/data_yper/yperv1/images/deleted")
+#
+#     shutil.move(
+#         f"/data_yper/yperv1/images/train/{fname}.jpg",
+#         f"/data_yper/yperv1/images/deleted/{fname}.jpg",
+#     )
+#     shutil.move(
+#         f"/data_yper/yperv1/labels/train/{fname}.txt",
+#         f"/data_yper/yperv1/labels/deleted/{fname}.txt",
+#     )
 #
 # with open("validation_list.txt", "r") as f:
 #     validation_list = f.readlines()
