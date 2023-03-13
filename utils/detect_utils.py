f469a46d65b1f45d7e188660fc236ee4043de850
angseung <swim@surromind.ai>
2023-03-13 16:47:27 +0900

    TODO: implement line separator...

--- utils/detect_utils.py
+++ utils/detect_utils.py
@@ -4,11 +4,11 @@ from .classes_map import class_labels
 from .augment_utils import label_voc2yolo, label_yolo2voc
 
 
-def angle_between(p1: Tuple[int, int], p2: Tuple[int, int]) -> int:
-    delta_x = abs(p1[0] - p2[0])
-    delta_y = abs(p1[0] - p2[0])
+def angle_between(p1: List[float], p2: List[float]) -> float:
+    delta_x = (p2[0] - p1[0])
+    delta_y = (p2[1] - p1[1])
 
-    return np.arctan2(delta_x, delta_y) * 180 / np.pi
+    return abs(np.arctan2(delta_y, delta_x) * 180 / np.pi)
 
 
 def read_bboxes(bboxes: np.ndarray, tolerance: Optional[float] = 0.3) -> str:
@@ -18,6 +18,7 @@ def read_bboxes(bboxes: np.ndarray, tolerance: Optional[float] = 0.3) -> str:
     is_multi_row = False
     plate_string = ""
     index_second_row = None
+    angular_thresh = 15.0
 
     # check plate has a row or 2 rows
     # bboxes_ytl = np.sort(bboxes[:, 1].detach().cpu().flatten())
@@ -34,13 +35,26 @@ def read_bboxes(bboxes: np.ndarray, tolerance: Optional[float] = 0.3) -> str:
     #         break
 
     bboxes_xtl = np.sort(bboxes[:, 0].detach().cpu().flatten())
+    bboxes_sequence = np.argsort(bboxes[:, 0].detach().cpu().flatten())
+    bboxes_reordered = bboxes[bboxes_sequence]
 
+    point_1 = bboxes_reordered[0][:2].tolist()
+    point_2 = bboxes_reordered[1][:2].tolist()
+    point_3 = bboxes_reordered[2][:2].tolist()
+
+    angle_1 = angle_between(point_1, point_2) > angular_thresh
+    angle_2 = angle_between(point_1, point_3) > angular_thresh
+    angle_3 = angle_between(point_2, point_3) > angular_thresh
+
+    is_multi_row = angle_1 or angle_2 or angle_3
+
+    base_ytl = 1.25 * bboxes[:, 1].detach().cpu().flatten().numpy().mean()
 
     # read plates
-    if is_multi_row and index_second_row:  # short plates
-        bboxes_h_thresh = int(bboxes_ytl[index_second_row])
-        bboxes_in_first_row = bboxes[bboxes[:, 1] <= bboxes_h_thresh]
-        bboxes_in_second_row = bboxes[bboxes[:, 1] > bboxes_h_thresh]
+    if is_multi_row:  # short plates
+        bbox_mask = bboxes[:, 3] > base_ytl
+        bboxes_in_second_row = bboxes[bbox_mask]
+        bboxes_in_first_row = bboxes[np.logical_not(bbox_mask)]
         plate_string_first_row = bboxes_to_string(bboxes_in_first_row)
         plate_string_second_row = bboxes_to_string(bboxes_in_second_row)
         plate_string = plate_string_first_row + plate_string_second_row
