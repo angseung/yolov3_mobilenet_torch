98534e3ea6a1ddee40c7870d84be331cad49c055
angseung <swim@surromind.ai>
2023-04-19 16:12:04 +0900

    Implementing static_quantizer
    TODO:
    1. Quantization fails when padding parameter given as string (https://github.com/pytorch/pytorch/issues/76304)
    2. implementing dynamic_quantizer for Conv layer, which is not supported natively in PyTorch (https://pytorch.org/docs/stable/quantization.html)
    3.Debug operation codes in skip connection
    4. SiLU Layer cannot forward quant tensor

--- quantize_test.py
+++ quantize_test.py
@@ -1,15 +1,25 @@
+import platform
 import torch
-from torchvision.models import resnet152
+from utils.quantization_utils import resnet152, resnet18
 from utils.quantization_utils import static_quantizer, QuantModel
+
+architecture = platform.uname().machine
+
+if architecture == "AMD64":
+    backend = "x86"
+elif architecture == "aarch64":
+    backend = "ARM64"
+else:
+    exit(1)
 
 # backend = 'x86'
 # qconfig = torch.quantization.get_default_qconfig(backend)
 # torch.backends.quantized.engine = backend
 
 # model = torch.load("yolov3.pt", map_location="cpu")["model"]
-model = resnet152()
+model = resnet18()
 model = QuantModel(model)
-model = static_quantizer(model, configs="x86").to("cpu").eval()
+model = static_quantizer(model, configs=backend).to("cpu").eval()
 
 x = torch.randn((1, 3, 320, 320)).to("cpu")
 # x = prepare(x)
--- utils/quantization_utils.py
+++ utils/quantization_utils.py
@@ -1,6 +1,7 @@
 from typing import *
 import torch
 from torch import nn as nn
+from torchvision.models.resnet import BasicBlock, Bottleneck, _resnet
 from torch.ao.quantization import quantize_dynamic
 from torch.ao.nn.quantized import Conv2d as qConv2d
 from torch.ao.quantization import qconfig
@@ -11,20 +12,44 @@ class M(nn.Module):
     def __init__(self, quantized=False):
         super().__init__()
         if quantized:
-            self.conv1 = qConv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)  # 224
-            self.conv2 = qConv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=False)  # 112
-            self.conv3 = qConv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False)  # 56
-            self.conv4 = qConv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False)  # 28
-            self.conv5 = qConv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)  # 14
-            self.conv6 = qConv2d(512, 1024, kernel_size=3, stride=2, padding=1, bias=False)  # 7
+            self.conv1 = qConv2d(
+                3, 32, kernel_size=3, stride=1, padding=1, bias=False
+            )  # 224
+            self.conv2 = qConv2d(
+                32, 64, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 112
+            self.conv3 = qConv2d(
+                64, 128, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 56
+            self.conv4 = qConv2d(
+                128, 256, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 28
+            self.conv5 = qConv2d(
+                256, 512, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 14
+            self.conv6 = qConv2d(
+                512, 1024, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 7
 
         else:
-            self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)  # 224
-            self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=False)  # 112
-            self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False)  # 56
-            self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False)  # 28
-            self.conv5 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False)  # 14
-            self.conv6 = nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1, bias=False)  # 7
+            self.conv1 = nn.Conv2d(
+                3, 32, kernel_size=3, stride=1, padding=1, bias=False
+            )  # 224
+            self.conv2 = nn.Conv2d(
+                32, 64, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 112
+            self.conv3 = nn.Conv2d(
+                64, 128, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 56
+            self.conv4 = nn.Conv2d(
+                128, 256, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 28
+            self.conv5 = nn.Conv2d(
+                256, 512, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 14
+            self.conv6 = nn.Conv2d(
+                512, 1024, kernel_size=3, stride=2, padding=1, bias=False
+            )  # 7
 
         self.avgpool = nn.AvgPool2d(kernel_size=7)
         self.fc = nn.Linear(1024, 10)
@@ -42,6 +67,62 @@ class M(nn.Module):
         return x
 
 
+class QuantBasicBlock(BasicBlock):
+    f_add = torch.nn.quantized.FloatFunctional()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.f_add.add(out, identity)
+        out = self.relu(out)
+
+        return out
+
+
+class QuantBottleNeck(Bottleneck):
+    f_add = torch.nn.quantized.FloatFunctional()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.f_add.add(out, identity)
+        out = self.relu(out)
+
+        return out
+
+
+def resnet18():
+    return _resnet(QuantBasicBlock, [2, 2, 2, 2], weights=None, progress=False)
+
+
+def resnet152():
+    return _resnet(QuantBottleNeck, [3, 8, 36, 3], weights=None, progress=False)
+
+
 def dynamic_quantizer(
     model: nn.Module, layers: List[type], dtype: torch.dtype
 ) -> nn.Module:
@@ -55,7 +136,9 @@ def dynamic_quantizer(
 
 
 def static_quantizer(
-        model: nn.Module, configs: Optional[Union[str, None]] = None, return_prepare: Optional[bool] = False
+    model: nn.Module,
+    configs: Optional[Union[str, None]] = None,
+    return_prepare: Optional[bool] = False,
 ) -> nn.Module:
     """
     https://pytorch.org/tutorials/advanced/static_quantization_tutorial.html
@@ -70,7 +153,7 @@ def static_quantizer(
     # model_fp32_fused = torch.ao.quantization.fuse_modules(model, [['conv', 'relu']])
     prepare = torch.ao.quantization.prepare(model)
     quantized_model = torch.ao.quantization.convert(prepare)
-    print('Post Training Quantization: Convert done')
+    print("Post Training Quantization: Convert done")
 
     return quantized_model
 
@@ -97,28 +180,6 @@ class QuantModel(nn.Module):
 if __name__ == "__main__":
     # create a model instance
     model_fp32 = M(quantized=False).to("cpu").eval()
-    model_int8 = dynamic_quantizer(
-        model=model_fp32, layers=[nn.Linear, nn.Conv2d, nn.AvgPool2d], dtype=torch.qint8
-    )
-    model_fp32.qconfig = torch.ao.quantization.default_qconfig
-    print(model_fp32.qconfig)
-    torch.ao.quantization.prepare(model_fp32, inplace=True)
-
-    # Calibrate first
-    print('Post Training Quantization Prepare: Inserting Observers')
-    print('\n Inverted Residual Block:After observer insertion \n\n', model_fp32.conv1)
-
-    # Calibrate with the training set
-    print('Post Training Quantization: Calibration done')
-
-    # Convert to quantized model
-    torch.ao.quantization.convert(model_fp32, inplace=True)
-    print('Post Training Quantization: Convert done')
-    print('\n Inverted Residual Block: After fusion and quantization, note fused modules: \n\n',model_fp32.conv1)
-
-    # run the model
-    input_fp32 = torch.randn(1, 3, 224, 224)
-    res = model_fp32(input_fp32)
-
-    # model = torch.load("../yolo.pt", map_location="cpu")
-    # model = static_quantizer(model, configs=None)
+    model = QuantModel(model=model_fp32)
+    dummy_input = torch.randn(1, 3, 224, 224)
+    dummy_output = model(dummy_input)
